7b9030366e878c29a04d7d1cb4c62c0d91f92302
Vihanga Heshan Edirisingha <vihangaheshan37@gmail.com>
2024-12-10 05:33:48 +0530

    Update Back-End End Point

--- TechMart/src/java/controller/LoadHomeView.java
+++ TechMart/src/java/controller/LoadHomeView.java
@@ -5,7 +5,6 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletException;
@@ -28,43 +27,45 @@ public class LoadHomeView extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Gson gson = new GsonBuilder().excludeFieldsWithModifiers().create();
         Session session = HibernateUtil.getSessionFactory().openSession();
-
+        
+        JsonObject responseObject = new JsonObject();
         List<String> validFilePaths = new ArrayList<>();
 
         Criteria productTable = session.createCriteria(Product.class);
         productTable.addOrder(Order.desc("dateTime"));
         productTable.setMaxResults(1);
-        Product latestProduct = (Product) productTable.list().get(0);
 
-        Criteria productsTable = session.createCriteria(Product.class);
-        productsTable.addOrder(Order.desc("dateTime"));
-        productsTable.setMaxResults(8);
-        List<Product> ProductList = productsTable.list();
+        if (!productTable.list().isEmpty()) {
+            Product latestProduct = (Product) productTable.list().get(0);
 
-        Criteria productHasFeatureTable = session.createCriteria(ProductHasFeatureList.class);
-        productHasFeatureTable.add(Restrictions.eq("product", latestProduct));
-        productHasFeatureTable.setMaxResults(8);
-        List<ProductHasFeatureList> productHasFeatureList = productHasFeatureTable.list();
+            Criteria productsTable = session.createCriteria(Product.class);
+            productsTable.addOrder(Order.desc("dateTime"));
+            productsTable.setMaxResults(8);
+            List<Product> ProductList = productsTable.list();
 
-        for (int i = 1; i <= 5; i++) {
-            String applicationPath = req.getServletContext().getRealPath("//img//product").replace("\\build\\web", "\\web");
-            String filePath = applicationPath + "//" + latestProduct.getTitle() + "_(" + latestProduct.getId() + ")_(" + i + ").jpg";
+            Criteria productHasFeatureTable = session.createCriteria(ProductHasFeatureList.class);
+            productHasFeatureTable.add(Restrictions.eq("product", latestProduct));
+            productHasFeatureTable.setMaxResults(8);
+            List<ProductHasFeatureList> productHasFeatureList = productHasFeatureTable.list();
 
-            File file = new File(filePath);
+            for (int i = 1; i <= 5; i++) {
+                String applicationPath = req.getServletContext().getRealPath("//img//product").replace("\\build\\web", "\\web");
+                String filePath = applicationPath + "//" + latestProduct.getTitle() + "_(" + latestProduct.getId() + ")_(" + i + ").jpg";
 
-            if (file.exists()) {
-                String imagePath = latestProduct.getTitle() + "_(" + latestProduct.getId() + ")_(" + i + ").jpg";
-                validFilePaths.add(imagePath);
-            } else {
-                break;
+                File file = new File(filePath);
+
+                if (file.exists()) {
+                    String imagePath = latestProduct.getTitle() + "_(" + latestProduct.getId() + ")_(" + i + ").jpg";
+                    validFilePaths.add(imagePath);
+                } else {
+                    break;
+                }
             }
+            responseObject.add("latestProduct", gson.toJsonTree(latestProduct));
+            responseObject.add("latestFeatureList", gson.toJsonTree(productHasFeatureList));
+            responseObject.add("ProductList", gson.toJsonTree(ProductList));
+            responseObject.add("imageList", gson.toJsonTree(validFilePaths));
         }
-
-        JsonObject responseObject = new JsonObject();
-        responseObject.add("latestProduct", gson.toJsonTree(latestProduct));
-        responseObject.add("latestFeatureList", gson.toJsonTree(productHasFeatureList));
-        responseObject.add("ProductList", gson.toJsonTree(ProductList));
-        responseObject.add("imageList", gson.toJsonTree(validFilePaths));
 
         resp.setContentType("application/json");
         resp.getWriter().write(gson.toJson(responseObject));
