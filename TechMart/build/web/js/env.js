15b5cad2b31f6e4ce88c90e7e981c95c3085e26b
Vihanga Heshan Edirisingha <vihangaheshan37@gmail.com>
2024-12-11 07:59:30 +0530

    Update DB, ENV, gitignore

--- TechMart/build/web/js/env.js
+++ TechMart/build/web/js/env.js
@@ -1,2 +1,2 @@
-export const merchantID = "1221196";
-export const merchantSecret = "NzcwNzM0NDkyNDA4NTQwMjkwNzE4MDM0NDA0MTE0MTM2OTA3OTk5";
+export const merchantID = "";
+export const merchantSecret = "";
