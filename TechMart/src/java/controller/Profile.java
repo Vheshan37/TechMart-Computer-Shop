3b3b58fdbc93e9bf67a9e9b6ccc03b30a3270cf9
Vihanga Heshan Edirisingha <vihangaheshan37@gmail.com>
2024-12-13 07:16:21 +0530

    Update Profile Client Interaction

--- TechMart/src/java/controller/Profile.java
+++ TechMart/src/java/controller/Profile.java
@@ -1,5 +1,7 @@
 package controller;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,23 +21,33 @@ public class Profile extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Session session = HibernateUtil.getSessionFactory().openSession();
+        Gson gson = new Gson();
 
         String action = req.getParameter("action");
+        JsonObject responseObj = new JsonObject();
         if (action.equals("get")) {
 
             if (req.getSession().getAttribute("tm_user") != null) {
+
                 User_DTO user = (User_DTO) req.getSession().getAttribute("tm_user");
+                responseObj.add("user", gson.toJsonTree(user));
 
                 Criteria userAddressTable = session.createCriteria(Address.class);
                 userAddressTable.add(Restrictions.eq("user", user));
 
                 if (!userAddressTable.list().isEmpty()) {
-                    
+                    Address address = (Address) userAddressTable.uniqueResult();
+                    responseObj.add("address", gson.toJsonTree(address));
                 }
             }
         } else {
             System.out.println(action);
         }
+
+        session.close();
+
+        resp.setContentType("application/json");
+        resp.getWriter().write(gson.toJson(responseObj));
 
     }
 }
--- TechMart/web/js/profile.js
+++ TechMart/web/js/profile.js
@@ -11,6 +11,13 @@ async function getProfileDetails() {
     if (response.ok) {
         console.log(response);
         const json = await response.json();
+        document.getElementById("first_name").value = json.user.first_name;
+        document.getElementById("last_name").value = json.user.last_name;
+        document.getElementById("mobile").value = json.user.mobile;
+        document.getElementById("email").value = json.user.email;
+        document.getElementById("line1").value = json.address.line1;
+        document.getElementById("line2").value = json.address.line2;
+        document.getElementById("postal_code").value = json.address.postalCode;
     } else {
         console.log("Response error!");
     }
