82eca0be9769ebe9898330aed55c39b7de716c72
Vihanga Heshan Edirisingha <vihangaheshan37@gmail.com>
2024-12-15 01:02:46 +0530

    Complete Checkout Process/ Cart Purchasing

--- TechMart/src/java/controller/Checkout.java
+++ TechMart/src/java/controller/Checkout.java
@@ -3,7 +3,6 @@ package controller;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.DecimalFormat;
 import java.util.Date;
 import java.util.List;
@@ -39,79 +38,26 @@ public class Checkout extends HttpServlet {
 
         // user login validation
         if (req.getSession().getAttribute("tm_user") != null) {
-            User_DTO userDTO = (User_DTO) req.getSession().getAttribute("tm_user");
-            User user = (User) session.get(User.class, userDTO.getId());
-            responseObject.add("user", gson.toJsonTree(user));
 
-            // address validation
-            Criteria userAddressCriteria = session.createCriteria(Address.class);
-            userAddressCriteria.add(Restrictions.eq("user", user));
-            Address userAddress = (Address) userAddressCriteria.uniqueResult();
+            User user = getUser(session, req, responseObject, gson); //get user            
+            Address userAddress = getUserAddress(session, user); // get user address
 
             if (userAddress != null) {
                 responseObject.addProperty("payment_status", true);
 
-                // load cart items
-                Criteria cartTable = session.createCriteria(Cart.class);
-                cartTable.add(Restrictions.eq("user", session.get(User.class, user.getId())));
-                List<Cart> cartList = cartTable.list();
+                List<Cart> cartList = getCartList(session, user); // get cart list
+                if (!cartList.isEmpty()) {
+                    OrderStatus orderStatus = getOrderStatus(session); // get order status
+                    Order order = createNewOrder(user, orderStatus, session); // create new order
 
-                // get order status
-                Criteria orderStatusCriteria = session.createCriteria(OrderStatus.class);
-                orderStatusCriteria.add(Restrictions.eq("status", "pending"));
-                OrderStatus orderStatus = (OrderStatus) orderStatusCriteria.uniqueResult();
+                    double cartAmount = 0;
+                    for (Cart cart : cartList) {
+                        cartAmount += cart.getProduct().getPrice() * cart.getQuantity();
+                        handleOrderItem(cart, order, session);
+                    }
 
-                // create new order
-                Order order = new Order();
-                order.setDateTime(new Date());
-                order.setUser(user);
-                order.setStatus(orderStatus);
-                session.save(order);
-
-                double cartAmount = 0;
-                for (Cart cart : cartList) {
-                    cartAmount += cart.getProduct().getPrice() * cart.getQuantity();
-
-                    // insert order items
-                    OrderItem orderItem = new OrderItem();
-                    orderItem.setProduct(cart.getProduct());
-                    orderItem.setQty(cart.getQuantity());
-                    orderItem.setOrder(order);
-                    session.save(orderItem);
-
-                    session.delete(cart);
+                    handlePaymentProcess(cartAmount, order, user, userAddress, responseObject); // payhere process
                 }
-
-                // payhere process
-                String formattedAmount = new DecimalFormat("0.00").format(cartAmount);
-                String orderIDString = String.valueOf(order.getId());
-                String currency = "LKR";
-                String merchantId = "1221196";
-                String hashCode = "NzcwNzM0NDkyNDA4NTQwMjkwNzE4MDM0NDA0MTE0MTM2OTA3OTk5";
-                String merchantSecret = PayHere.generateMD5(hashCode);
-
-                String md5Hash = PayHere.generateMD5(merchantId + orderIDString + formattedAmount + currency + merchantSecret);
-
-                JsonObject payhereData = new JsonObject();
-                payhereData.addProperty("merchant_id", merchantId);
-                payhereData.addProperty("sandbox", true);
-                payhereData.addProperty("return_url", "index.html");
-                payhereData.addProperty("cancel_url", "index.html");
-                payhereData.addProperty("notify_url", "index.html");
-                payhereData.addProperty("first_name", userDTO.getFirst_name());
-                payhereData.addProperty("last_name", userDTO.getLast_name());
-                payhereData.addProperty("email", userDTO.getEmail());
-                payhereData.addProperty("phone", userDTO.getMobile());
-                payhereData.addProperty("address", userAddress.getLine1() + ", " + userAddress.getLine2() + "." + userAddress.getCity().getCity());
-                payhereData.addProperty("city", userAddress.getCity().getCity());
-                payhereData.addProperty("country", "Sri Lanka");
-                payhereData.addProperty("order_id", orderIDString);
-                payhereData.addProperty("items", "Cart Items. Order: " + order.getId());
-                payhereData.addProperty("currency", currency);
-                payhereData.addProperty("amount", formattedAmount);
-                payhereData.addProperty("hash", md5Hash);                
-                
-                responseObject.add("payhere_data", payhereData);
             } else {
                 responseObject.addProperty("login_status", "Incomplete profile details");
             }
@@ -126,4 +72,84 @@ public class Checkout extends HttpServlet {
         resp.setContentType("application/json");
         resp.getWriter().write(gson.toJson(responseObject));
     }
+
+    public User getUser(Session session, HttpServletRequest req, JsonObject responseObject, Gson gson) {
+        User_DTO userDTO = (User_DTO) req.getSession().getAttribute("tm_user");
+        User user = (User) session.get(User.class, userDTO.getId());
+        responseObject.add("user", gson.toJsonTree(user));
+
+        return user;
+    }
+
+    public Address getUserAddress(Session session, User user) {
+        Criteria userAddressCriteria = session.createCriteria(Address.class);
+        userAddressCriteria.add(Restrictions.eq("user", user));
+        Address userAddress = (Address) userAddressCriteria.uniqueResult();
+        return userAddress;
+    }
+
+    public List<Cart> getCartList(Session session, User user) {
+        Criteria cartTable = session.createCriteria(Cart.class);
+        cartTable.add(Restrictions.eq("user", session.get(User.class, user.getId())));
+        List<Cart> cartList = cartTable.list();
+        return cartList;
+    }
+
+    public OrderStatus getOrderStatus(Session session) {
+        Criteria orderStatusCriteria = session.createCriteria(OrderStatus.class);
+        orderStatusCriteria.add(Restrictions.eq("status", "pending"));
+        OrderStatus orderStatus = (OrderStatus) orderStatusCriteria.uniqueResult();
+        return orderStatus;
+    }
+
+    public Order createNewOrder(User user, OrderStatus orderStatus, Session session) {
+        Order order = new Order();
+        order.setDateTime(new Date());
+        order.setUser(user);
+        order.setStatus(orderStatus);
+        session.save(order);
+        return order;
+    }
+
+    public void handleOrderItem(Cart cart, Order order, Session session) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setProduct(cart.getProduct());
+        orderItem.setQty(cart.getQuantity());
+        orderItem.setOrder(order);
+        session.save(orderItem);
+        session.delete(cart);
+    }
+
+    public void handlePaymentProcess(double cartAmount, Order order, User user, Address userAddress, JsonObject responseObject) {
+        String formattedAmount = new DecimalFormat("0.00").format(cartAmount);
+        String orderIDString = String.valueOf(order.getId());
+        String currency = "LKR";
+        String merchantId = "1221196";
+        String hashCode = "NzcwNzM0NDkyNDA4NTQwMjkwNzE4MDM0NDA0MTE0MTM2OTA3OTk5";
+        String merchantSecret = PayHere.generateMD5(hashCode);
+
+        String md5Hash = PayHere.generateMD5(merchantId + orderIDString + formattedAmount + currency + merchantSecret);
+
+        JsonObject payhereData = new JsonObject();
+        payhereData.addProperty("merchant_id", merchantId);
+        payhereData.addProperty("sandbox", true);
+        payhereData.addProperty("return_url", "index.html");
+        payhereData.addProperty("cancel_url", "index.html");
+        payhereData.addProperty("notify_url", "index.html");
+        payhereData.addProperty("first_name", user.getFirst_name());
+        payhereData.addProperty("last_name", user.getLast_name());
+        payhereData.addProperty("email", user.getEmail());
+        payhereData.addProperty("phone", user.getMobile());
+        payhereData.addProperty("address", userAddress.getLine1() + ", " + userAddress.getLine2() + "." + userAddress.getCity().getCity());
+        payhereData.addProperty("city", userAddress.getCity().getCity());
+        payhereData.addProperty("country", "Sri Lanka");
+        payhereData.addProperty("order_id", orderIDString);
+        payhereData.addProperty("items", "Cart Items. Order: " + order.getId());
+        payhereData.addProperty("currency", currency);
+        payhereData.addProperty("amount", formattedAmount);
+        payhereData.addProperty("hash", md5Hash);
+
+        responseObject.add("payhere_data", payhereData);
+    }
+
 }
