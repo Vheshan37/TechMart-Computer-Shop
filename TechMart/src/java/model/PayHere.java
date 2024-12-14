e9a9572af4d09622eaa3bc3f10b2172f5fbf79b7
Vihanga Heshan Edirisingha <vihangaheshan37@gmail.com>
2024-12-14 10:44:01 +0530

    Update DB model & Payment Process

--- TechMart/src/java/model/PayHere.java
+++ TechMart/src/java/model/PayHere.java
@@ -1,22 +1,16 @@
 package model;
 
-import java.math.BigInteger;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import javax.xml.bind.DatatypeConverter;
 
 public class PayHere {
 
-    public static String getMd5(String input) {
+    public static String generateMD5(String input) {
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] messageDigest = md.digest(input.getBytes());
-            BigInteger no = new BigInteger(1, messageDigest);
-            String hashtext = no.toString(16);
-            while (hashtext.length() < 32) {
-                hashtext = "0" + hashtext;
-            }
-            return hashtext.toUpperCase();
-        } catch (NoSuchAlgorithmException e) {
+            byte[] digest = md.digest(input.getBytes());
+            return DatatypeConverter.printHexBinary(digest).toUpperCase();
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
