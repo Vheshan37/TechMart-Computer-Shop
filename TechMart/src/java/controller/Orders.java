4fb68ed2f1234930fd2d83ee216d4ba3f92fce9b
Vihanga Heshan Edirisingha <vihangaheshan37@gmail.com>
2024-12-15 09:09:51 +0530

    Begin Order Tracking process

---
+++ TechMart/src/java/controller/Orders.java
@@ -0,0 +1,38 @@
+package controller;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@WebServlet(name = "Orders", urlPatterns = {"/Orders"})
+public class Orders extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // initial stage
+        Gson gson = new Gson();
+        JsonObject responseObject = new JsonObject();
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        // processing stage
+        Transaction transaction = session.beginTransaction();
+
+        // code here...
+        
+        // finalizing stage
+        transaction.commit();
+        session.close();
+
+        resp.setContentType("application/json");
+        resp.getWriter().write(gson.toJson(responseObject));
+    }
+
+}
